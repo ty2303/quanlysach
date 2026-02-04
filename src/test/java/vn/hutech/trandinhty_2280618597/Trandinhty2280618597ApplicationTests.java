ad29f162d142ffbc095dda3b8150dae268907e5b
ty2303 <trandinhty23@gmail.com>
2026-02-04 19:05:23 +0700

    khoi tạo

---
+++ src/test/java/vn/hutech/trandinhty_2280618597/Trandinhty2280618597ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hutech.trandinhty_2280618597;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trandinhty2280618597ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
